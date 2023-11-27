acae24b86b25cc10e51a5f3b3d0e6df0049dbaba
Simon Chervenak <sarras305@gmail.com>
2023-11-27 00:04:36 -0500

    test

---
+++ moev/moev/moevApp.swift
@@ -0,0 +1,17 @@
+//
+//  moevApp.swift
+//  moev
+//
+//  Created by Simon Chervenak on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct moevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
