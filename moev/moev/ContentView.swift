45fcf6b4a8ab0279956d01fa629d43d671346d18
Simon Chervenak <sarras305@gmail.com>
2023-11-27 23:24:45 -0500

    selection baby

--- moev/moev/ContentView.swift
+++ moev/moev/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 import MapKit
 
+struct Place: Identifiable {
+    var id = UUID()
+    
+    var name: String
+}
+
 struct ContentView: View {
-    @State private var searchText: String = ""
+    @State public var searchText: String = ""
+    
+    @State public var selection: UUID?
+
+    
+    let possibilities = [Place(name: "place 1"), Place(name: "Place 2")]
+    
     var body: some View {
         VStack {
             HStack {
                 TextField("Next city...", text: $searchText)
                 Image(systemName: "location.magnifyingglass")
             }
+            
+            List(possibilities, selection: $selection) {
+                Text($0.name)
+            }
+            
             Map {
                 
             }
