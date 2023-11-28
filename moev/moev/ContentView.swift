33fc42fdf301ca10b4a999107fea0890742f6102
Simon Chervenak <sarras305@gmail.com>
2023-11-28 11:30:12 -0500

    implement possibilities

--- moev/moev/ContentView.swift
+++ moev/moev/ContentView.swift
@@ -19,29 +19,37 @@ struct ContentView: View {
     
     @State public var selection: UUID?
 
-    let possibilities = [Place(name: "place 1"), Place(name: "Place 2")]
+    @State public var possibilities: [Place] = []
 
     var body: some View {
         VStack {
-            HStack {
-                TextField("Next city...", text: $searchText)
-                    .textFieldStyle(.roundedBorder)
-                    .onChange(of: searchText) { (oldValue, newValue) in
-                        APIHandler.shared.autocomplete(query: searchText) { data, response, error in
-                            print(data)
-                            print(response)
-                            print(error)
-                        }
-                    }
-                Image(systemName: "location.magnifyingglass")
-            }
-            
             ZStack {
                 Map {
                     
                 }
                 
                 VStack(alignment: .leading) {
+                    HStack {
+                        TextField("Next city...", text: $searchText)
+                            .textFieldStyle(.roundedBorder)
+                            .onChange(of: searchText) { (oldValue, newValue) in
+                                APIHandler.shared.autocomplete(query: searchText) { data, response, error in
+                                    
+                                    guard let d = data else {
+                                        print(error)
+                                        return
+                                    }
+                                    
+                                    let cities = try! JSONSerialization.jsonObject(with: d, options: []) as! [String : Any]
+                                    
+                                    possibilities = (cities["predictions"] as! [[String: Any]]).map { city in
+                                        return Place(name: city["description"] as! String)
+                                    }
+                                }
+                            }
+                        Image(systemName: "location.magnifyingglass")
+                    }
+                    
                     ForEach(possibilities) { i in
                         HStack {
                             Text(i.name)
