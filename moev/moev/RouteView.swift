6a74fb7bfc3656805b20d203f97ba5fe7f45ca3a
Simon Chervenak <sarras305@gmail.com>
2023-12-16 21:00:08 -0500

    fixing some hastily written methods

--- moev/moev/RouteView.swift
+++ moev/moev/RouteView.swift
@@ -12,20 +12,19 @@ struct RouteView: View {
     @State public var route: Route
     
     var body: some View {
-//        HStack {
-            ForEach(route.legs ?? []) { leg in
-                if let steps = leg.steps {
-                    stepsList(steps)
-                }
+        ForEach(route.legs ?? []) { leg in
+            if let steps = leg.steps {
+                stepsList(steps)
             }
-//        }
+        }
     }
     
     func stepsList(_ steps: [RouteLegStep]) -> some View {
         return HStack {
             ForEach(combineWalks(steps: steps, route: route)) { step in
                 stepsView(step)
-                    .offset(x: CGFloat(xposition(for: step)))
+//                    .offset(x: CGFloat(xposition(for: step)))
+                    .position(x: CGFloat(xposition(for: step)), y: 10)
             }
         }
     }
@@ -37,8 +36,6 @@ struct RouteView: View {
                     .frame(width: CGFloat(width(for: step)))
                     .border(.black, width: 2)
             }
-
-//            Text(String(step.totalDuration))
         }
     }
 }
--- moev/moev/UIUtils.swift
+++ moev/moev/UIUtils.swift
@@ -15,40 +15,43 @@ extension UIColor {
   }
 }
 
-func _hm(date: Date) -> (Int, Int) {
+func _hm(date: Date, clamp: Bool) -> (Int, Int) {
     let cal = Calendar.current
     var hour = cal.component(.hour, from: date)
     var minutes = cal.component(.minute, from: date)
-    if minutes > 30 {
-        hour += 1
-        minutes = 0
-    } else {
-        minutes = 30
+    if clamp {
+        if minutes > 30 {
+            hour += 1
+            minutes = 0
+        } else {
+            minutes = 30
+        }
     }
     return (hour, minutes)
 }
 
-func _hm(plus: Int) -> (Int, Int) {
+func _hm(plus: Int, clamp: Bool) -> (Int, Int) {
     let date = Date(timeIntervalSinceNow: TimeInterval(plus * 30 * 60))
-    return _hm(date: date)
+    return _hm(date: date, clamp: clamp)
 }
 
 func time(plus: Int) -> String {
-    let (hour, minutes) = _hm(plus: plus)
+    let (hour, minutes) = _hm(plus: plus, clamp: true)
     let padh = String(repeating: "0", count: hour < 10 ? 1 : 0)
     let padm = String(repeating: "0", count: minutes < 10 ? 1 : 0)
     return "\(padh)\(hour):\(padm)\(minutes)"
 }
 
 func time(plus: Int) -> Int {
-    let (h1, m1) = _hm(date: Date())
-    let (h2, m2) = _hm(plus: plus)
+    let (h1, m1) = _hm(date: Date(), clamp: true)
+    let (h2, m2) = _hm(plus: plus, clamp: true)
     return (h2 - h1) * 3600 + (m2 - m1) * 60
 }
 
 func time(date: Date) -> Int {
-    let (h1, m1) = _hm(date: Date())
-    let (h2, m2) = _hm(date: date)
+    let (h1, m1) = _hm(date: Date.now, clamp: false)
+    let (h2, m2) = _hm(date: date, clamp: false)
+    print(h1, m1, h2, m2, Date.now, date, (h2 - h1) * 3600 + (m2 - m1) * 60)
     return (h2 - h1) * 3600 + (m2 - m1) * 60
 }
 
@@ -56,10 +59,7 @@ func time(timestamp: String?) -> Int? {
     if timestamp == nil {
         return nil
     }
-    let formatter = DateFormatter()
-    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-    let localDate = formatter.date(from: timestamp!)
-    return localDate == nil ? nil : time(date: localDate!)
+    return time(date: date(from: timestamp!)!)
 }
 
 func xposition(for time: Int) -> Int {
