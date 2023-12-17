d0f9064832cb27fd9750e5c10ff4855e05c178ba
Simon Chervenak <sarras305@gmail.com>
2023-12-16 23:00:11 -0500

    add transit info

--- moev/moev/RouteView.swift
+++ moev/moev/RouteView.swift
@@ -30,7 +30,17 @@ struct RouteView: View {
     
     func stepsView(_ step: CombinedStep) -> some View {
         return VStack {
-            if let tm = step.travelMode {
+            if let td = step.transitDetails {
+                Text(td.transitLine!.nameShort!)
+//                    .padding()
+                    .background(Color(hex: td.transitLine!.color!)) // Set background color using hex value
+                    .foregroundColor(Color(hex: td.transitLine!.textColor!)) // Set text color using hex value
+                    .frame(width: CGFloat(width(for: step)))
+                    .border(.black, width: 2)
+                    .lineLimit(1)
+                    .font(.system(size: 12))
+            }
+            else if let tm = step.travelMode {
                 tm.to_swiftui_image()
                     .frame(width: CGFloat(width(for: step)))
                     .border(.black, width: 2)
